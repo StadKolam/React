e0e7e6e1f28f4f9b65fa7a3604459da01818dc48
StadKolam <szecsitisza.adam@gmail.com>
2019-11-05 12:35:24 +0100

    Updates

---
+++ precache-manifest.4e9c3eec6f3bc7443e9d77f2ff41b908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24fb15b7c840533a1c22b7d66a61efa",
+    "url": "/React/RoboFriend/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/React/RoboFriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0b5ac4c86598f28851d0",
+    "url": "/React/RoboFriend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/React/RoboFriend/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "0b5ac4c86598f28851d0",
+    "url": "/React/RoboFriend/static/js/main.0d4df901.chunk.js"
+  },
+  {
+    "revision": "7c4076db81edee688797",
+    "url": "/React/RoboFriend/static/js/runtime-main.68590119.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React/RoboFriend/precache-manifest.4e9c3eec6f3bc7443e9d77f2ff41b908.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React/RoboFriend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
